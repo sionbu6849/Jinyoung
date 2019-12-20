6f70fd686a0d6a3034dabe5d18e0d289c0a17b6f
CME <CME@DESKTOP-B24ATHH>
2019-12-20 12:07:35 +0900

    modi

--- test/src/backjoon/back1075.java
+++ test/src/backjoon/back1075.java
@@ -1,9 +1,15 @@
 package backjoon;
 
+import java.util.Scanner;
+
 public class back1075 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int n;
+		int f;
+		
+		Scanner scn = new Scanner(System.in);
 
 	}
 
