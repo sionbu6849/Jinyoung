53a227bf38a6ebab31182f8701a61800428c6006
CME <CME@DESKTOP-B24ATHH>
2019-12-20 11:05:12 +0900

    bac1075

---
+++ test/src/test/bac1075.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.util.Scanner;
+
+public class bac1075 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		int n = 0;
+		int f = 0;
+		
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("두수를입력하세요.");
+
+		/*
+		 * n = scn.nextInt(); f = scn.nextInt();
+		 */
+		n = (int)(Math.random()*2000000000+100);
+		f = (int)(Math.random()*100+1);
+		
+			  System.out.println(" n= " + n + " f = " + f);
+
+	
+	}
+}
